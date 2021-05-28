374bc3b0514ca5909a1548800bbe564f317497a1
r3zafa <r3zafaraji@gmail.com>
2021-05-28 05:11:01 +0200

    section 3 , 28-05-2021

--- section3/index3.php
+++ section3/index3.php
@@ -18,7 +18,7 @@
             </svg>
             Php eLearning
         </a>
-        <h4 class="text-center text-warning"> { Php playground } </h4>
+        <h4 class="text-center text-warning"> { Php playground -2 } </h4>
         <a class="navbar-brand btn btn-outline-warning ml-5 mr-5" href="../index.php" type="button">
             <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-arrow-left-square" viewBox="0 0 19 20">
                 <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm11.5 5.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
@@ -27,22 +27,70 @@
         </a>
     </nav>
     <br>
-    <main role="main" class="container bg-light mt-3">
+    <main role="main" class="container bg-light mt-5">
         <div class="row g-3 g-lg-4 pb-5 px-5 text-dark text-center">
 
             <div class="col-12 col-lg-12">
-                <div class="card border-warning py-5 px-5">
-                    <h2 class="mt-2">Strings in php</h2>
+                <div class="card border-warning border-3 py-5 px-5">
+                    <h2 class="mt-2">Inside Job | Pointers</h2>
                     <hr>
                     <?php
-                    $color = "red";
-                    $shirt = "sleev-less";
-                    $combination = $color . " " . $shirt; //cobination of variables
-                    echo "The combination of 2 strings color + shirt is: " . $combination;
-                    echo "<br> different between double cotations and single cotations <br>";
-                    echo " double cotations of echo color: $color <br>";
-                    echo ' single cotations of echo color: $color <br>';
-                    echo "{$combination} combination echo is here again but using {} <br>";
+                    $Numbers = array(8, 23, 344, 566, 33, 111, 888, 666);
+                    print_r($Numbers);
+                    ?>
+                    <hr>
+                    <?php
+                    echo "Current pointer place is --> " . current($Numbers) . "<br><hr>";
+                    next($Numbers);
+                    echo "Next pointer place is: " . current($Numbers) . "<br><hr>"; // we can do it more than 1 times in row
+                    next($Numbers);
+                    next($Numbers);
+                    echo "2xNext pointer place is: " . current($Numbers) . "<br><hr>"; // we can do it more than 1 times in row
+                    reset($Numbers);
+                    echo "using reset() pointer go back to start point --> " . current($Numbers) . "<br><hr>";
+                    end($Numbers);
+                    echo "using end() pointer go  to end point --> " . current($Numbers) . "<br><hr>";
+                    ?>
+                </div>
+            </div>
+
+            <div class="col-12 col-lg-12">
+                <div class="card border-warning border-3 py-5 px-5">
+                    <h2 class="mt-2">If Statements</h2>
+                    <hr>
+                    <p>weather = sunny with if else Statement</p>
+                    <?php
+                    $weather = "Sunny";
+                    if ($weather == "Sunny") {
+                        echo "weather is pleasent <br><hr>";
+                    } else {
+                        echo "it's not pleasent <br><hr> ";
+                    }
+                    ?>
+                    <p>weather2 = rainy with if if Statement</p>
+                    <?php
+                    $weather2 = "rainy";
+                    if ($weather2 == "Sunny") {
+                        echo "weather is pleasent <br><hr>";
+                    }
+                    if ($weather2 != "Sunny") {
+                        echo "it's not pleasent <br><hr> ";
+                    }
+                    ?>
+
+                    <p>weather3 = snow with if ... elseif ... elseif Statement</p>
+                    <?php
+                    $weather3 = "cloudy";
+                    if ($weather3 == "Sunny") {
+                        echo "weather is pleasent <br><hr>";
+                    } elseif ($weather3 == "rainy") {
+                        echo "it's raining <br><hr> ";
+                    } elseif ($weather3 == "cloudy") {
+                        echo "it's cloudy out there <br><hr> ";
+                    }
+                    ?>
+                    <?php
+
                     ?>
                 </div>
             </div>
